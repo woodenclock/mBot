2ba4d99e3bcc83a75851eadd625848e720ae745f
mudkip8 <62732965+mudkip8@users.noreply.github.com>
2020-10-22 15:37:41 +0800

    Update code.c

--- code.c
+++ code.c
@@ -1,3 +1,64 @@
-print("HI CAN YOU SEE THIS?");
+/*
+ * mBot code for Group 3a2
+ *
+ * Members:
+ * Sung Min
+ * Kishor
+ * Svetha
+ * Alvin
+ */
 
-hello young one
+//define the required variables here
+
+
+//turn right
+void right_turn(){
+  
+}
+
+//turn left
+void left_turn(){
+  
+}
+
+//detect black strip
+void black_strip(){
+  //stop
+}
+
+//ultrasound(distance in front)
+float dist_front() {
+  float dist = ;
+  return dist;
+}
+
+//red (left turn)
+void red_task(){
+  
+}
+
+//green (right turn)
+void green_task(){
+  
+}
+
+//yellow (U-turn within same grid)
+void yellow_task(){
+  
+}
+
+//purple (2x left in 2 grid)
+void purple_task(){
+  
+}
+
+//light blue (2x right in 2 grid)
+void lightblue_task(){
+  
+}
+
+//black (make noise)
+void black_task(){
+  
+}
+
