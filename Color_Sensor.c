efcb27027f0df2447912088910d56d82de175938
mudkip8 <62732965+mudkip8@users.noreply.github.com>
2020-10-27 14:18:07 +0800

    Update Color_Sensor.c

--- Color_Sensor.c
+++ Color_Sensor.c
@@ -1,15 +1,14 @@
 #include "MeMCore.h"
 #include "Wire.h"
 
-MeColorSensor colorsensor(PORT_1);
+MeColorSensor colorsensor(PORT_4);
 
-uint8_t colorresult;
-uint16_t redvalue=0,greenvalue=0,bluevalue=0,colorvalue=0;
-uint8_t grayscale = 0;
-long systime = 0,colorcode=0;
+//initialise the color values to 0.
+uint16_t redvalue=0, greenvalue=0, bluevalue=0;
+
 void setup() {
-  // put your setup code here, to run once:
-  Serial.begin(115200);
+  //CALIBRATION STEPS
+  Serial.begin(9600);
   colorsensor.SensorInit();
   systime = millis();
 }
@@ -18,55 +17,8 @@ void loop() {
   // put your main code here, to run repeatedly:
 while(1)
 {
-    colorresult = colorsensor.Returnresult();
     redvalue   = colorsensor.ReturnRedData();
     greenvalue = colorsensor.ReturnGreenData();
     bluevalue  = colorsensor.ReturnBlueData();
-    colorvalue = colorsensor.ReturnColorData();
-    colorcode = colorsensor.ReturnColorCode();//RGB24code
-    grayscale  = colorsensor.ReturnGrayscale();
-
-    Serial.print("R:");
-    Serial.print(redvalue);
-    Serial.print("\t");
-    Serial.print("G:");
-    Serial.print(greenvalue);
-    Serial.print("\t");
-    Serial.print("B:");
-    Serial.print(bluevalue);
-    Serial.print("\t");
-    Serial.print("C:");
-    Serial.print(colorvalue);
-    Serial.print("\t");
-    Serial.print("color:");
-    switch(colorresult)
-    {
-      case BLACK:
-      Serial.print("BLACK");
-      break;
-      case BLUE:
-      Serial.print("BLUE");
-      break;
-      case YELLOW:
-      Serial.print("YELLOW");
-      break;
-      case GREEN:
-      Serial.print("GREEN");
-      break;
-      case RED:
-      Serial.print("RED");
-      break;
-      case WHITE:
-      Serial.print("WHITE");
-      break;
-      default:
-      break;    
-     }
-    Serial.print("\t");
-    Serial.print("code:");
-    Serial.print(colorcode,HEX);
-    Serial.print("\t");
-    Serial.print("grayscale:");
-    Serial.println(grayscale);
-  }
+   }
 }
